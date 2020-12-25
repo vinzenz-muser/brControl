56089ea87615957d95929e4988b1a242cfc183d5
Vinzenz <vinzenz@muser.io>
2020-12-25 18:49:49 +0100

    Fixed config a little

--- config.py
+++ config.py
@@ -2,5 +2,7 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///./database.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    HOST="0.0.0.0"
     DEBUG=True
